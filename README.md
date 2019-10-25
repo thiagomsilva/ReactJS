51a61f8b75010b464fd42cb4f36e942c90501fbc
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2019-10-25 01:35:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![](https://blog.fellyph.com.br/wp-content/uploads/2016/06/react-js.png)
+
+# ReactJS
+
+**Estudos sobre o ReactJS**
---
+++ src/components/TechList.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import TechItem from "./TechItem";
+
+class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: []
+  };
+  // É executado assim que o componente aparece na tela
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // É executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  // É executdo quando o componente deixa de existir
+  componentWillUnmount() {}
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
